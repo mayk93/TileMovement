35298b923b7f21b0105fe3782cf91729aaaf8fe4
mayk93 <mihai.mandrescu@gmail.com>
2015-01-26 23:16:46 +0200

    Tile Movement
    Preliminaries for tile movement.
    Created Tile class, serializable and the Tile Map.

---
+++ TileMap.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileMap : MonoBehaviour {
+
+	public TileType[] tileTypes;
+	int[,] tiles;
+
+	int mapSizeX = 10;
+	int mapSizeY = 10;
+
+	// Use this for initialization
+	void Start () {
+		tiles = new int[mapSizeX,mapSizeY];
+
+		for(int x = 0; x < mapSizeX; x++)
+		{
+			for (int y = 0; y < mapSizeY; y++)
+			{
+				tiles[x,y] = 0;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ TileType.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+
+public class TileType {
+
+	public string name;
+	public GameObject tilleVisualPrefab;
+
+	//public bool isWalkable = true;
+}
