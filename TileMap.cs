556536aae09c6f963c97020a3e1f7e02ef42317c
mayk93 <mihai.mandrescu@gmail.com>
2015-01-26 23:27:09 +0200

    Small Example
    Instantiated a "mountain" range to test tile implementation.
    
    Needs rework.

--- TileMap.cs
+++ TileMap.cs
@@ -10,7 +10,8 @@ public class TileMap : MonoBehaviour {
 	int mapSizeY = 10;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		tiles = new int[mapSizeX,mapSizeY];
 
 		for(int x = 0; x < mapSizeX; x++)
@@ -20,10 +21,36 @@ public class TileMap : MonoBehaviour {
 				tiles[x,y] = 0;
 			}
 		}
+
+		tiles [0, 3] = 2;
+		tiles [0, 2] = 2;
+		tiles [0, 1] = 2;
+		tiles [0, 0] = 2;
+
+		tiles [1, 0] = 2;
+		tiles [2, 0] = 2;
+
+		tiles [3, 3] = 2;
+		tiles [3, 2] = 2;
+		tiles [3, 1] = 2;
+		tiles [3, 0] = 2;
+
+		GenerateMapVisuals ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void GenerateMapVisuals()
+	{
+		for(int x = 0; x < mapSizeX; x++)
+		{
+			for (int y = 0; y < mapSizeY; y++)
+			{
+				Instantiate( tileTypes[tiles[x,y]].tilleVisualPrefab , new Vector3(x,y,0), Quaternion.identity);
+			}
+		}
 	}
 }
